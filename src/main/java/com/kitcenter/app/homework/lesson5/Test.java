ba02570a3d7c86183fface81e7f6262c33be5b27
Shroet <velikokhatsky@gmail.com>
2017-09-16 14:03:55 +0300

    Class test added

---
+++ src/main/java/com/kitcenter/app/homework/lesson5/Test.java
@@ -0,0 +1,10 @@
+package com.kitcenter.app.homework.lesson5;
+
+/**
+ * Created by Анатолий on 16.09.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("second");
+    }
+}
